90bdae0fdcfa0e1a187b933896689bd2776fb66e
Rishikesh Kasireddy <rishikeshkasireddy@gmail.com>
2025-01-25 02:43:27 +0530

    Input file read

--- main.c
+++ main.c
@@ -1,34 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <string.h>
+#include <stdbool.h>
 #include <pthread.h>
 #include <time.h>
 #include <unistd.h>
 
+int n,k;
+int **sudoku;
 int flag = 0; //For implementing early termination
 
 
-void readInputFromFile(const char *filename,int ***grid, int *n,int *k){
+void readInputFromFile(const char *filename){
     FILE *file = fopen(filename,"r");
     if(file==NULL){
         printf("Error opening the file\n");
         exit(EXIT_FAILURE);
     }
 
-    if (fscanf(file, "%d %d", k, n) != 2) {
+    if (fscanf(file, "%d %d", &k, &n) != 2) {
         fprintf(stderr, "Invalid format in the first line of the file\n");
         fclose(file);
         exit(EXIT_FAILURE);
     }
 
-    *grid = malloc(*n * sizeof(int *));
-    for (int i = 0; i < *n; i++){
-        (*grid)[i] = malloc(*n * sizeof(int));
+    sudoku = malloc(n * sizeof(int *));
+    for(int i = 0; i < n; i++){
+        sudoku[i] = malloc(n * sizeof(int));
     }
 
-    for(int i=0;i<*n;i++){
-        for(int j=0;j<*n;j++){
-            if(fscanf(file,"%d",&(*grid)[i][j]) != 1){         // Reads an integer and stores it in the grid
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(fscanf(file,"%d",&sudoku[i][j]) != 1){         // Reads an integer and stores it in the grid
                 fprintf(stderr, "Invalid input in file\n");
                 fclose(file);  
                 exit(EXIT_FAILURE);
@@ -47,17 +51,18 @@ void printSudoku(int **grid,int n){
     }
 }
 
+void chunkMethod(){
+    int chunk_size = n/k;
+    int grid_chunks = n/((int)sqrt(n));
+}
 
 int main(){
     char *filename = "input.txt";
 
-    int n,k;
-    int **grid;
-
     //Reading input from sudoku_input.txt
-    readInputFromFile(filename,&grid,&n,&k);
+    readInputFromFile(filename);
 
     printf("Number of threads: %d\n",k);
     printf("Sudoku grid size: %d x %d\n",n,n);
-    printSudoku(grid,n);
+    printSudoku(sudoku,n);
 }
